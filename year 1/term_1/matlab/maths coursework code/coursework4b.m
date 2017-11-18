16107e7db9a611d49a0a3d2335191509e2f84859
ammar903@gmail.com <ammar903@gmail.com>
2017-11-18 09:19:40 +0000

    moving files

--- year 1/term_1/matlab/maths coursework code/coursework4b.m
+++
@@ -1,9 +0,0 @@
-clear
-clc
-
-data = [1460 1240 1230 1270 861 1355 612 822 1370 1380]
-
-dataM = mean(data)
-
-sd = sqrt((sum((data-mean(data)).^2)/(numel(data)-1)))
-
--- year 1/term_1/matlab/maths coursework code/coursework4c.m
+++
@@ -1,9 +0,0 @@
-clear
-clc
-
-data = [810 735 259 1290 1125 528 622 468 664 717]
-
-dataM = mean(data)
-
-sd = sqrt((sum((data-mean(data)).^2)/(numel(data)-1)))
-
